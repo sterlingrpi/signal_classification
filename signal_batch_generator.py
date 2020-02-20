39310b73d5da4e13379f51f93a2bb3933f6ad8c5
snesbitt <sterling.nesbitt@gmail.com>
2020-02-20 13:32:42 -0500

    added amplitude and pitch shifting to data generator. Retrained

--- signal_batch_generator.py
+++ signal_batch_generator.py
@@ -4,9 +4,25 @@ import numpy as np
 def get_data_sample(data, data_length, class_number):
     start = random.randint(0, np.shape(data)[1] - data_length)
     end = start + data_length
-    data_slice = data[class_number][start:end]
+    data_sample = data[class_number][start:end]
+    #data_sample = pitch_shift(data_sample, shift=0.1)
+    data_sample = amplitude_adjust(data_sample, amplitude=0.5)
+    return data_sample
 
-    return data_slice
+def pitch_shift(data_sample, shift):
+    data_sample_mean = np.mean(data_sample)
+    data_sample -= data_sample_mean #remove DC offset
+    yf = np.fft.fft(data_sample)
+    len_yf = len(yf)
+    shift = int(shift*len_yf*(random.random() - 0.5))
+    yf = np.roll(yf, shift)
+    data_sample = np.fft.irfft(yf, axis=0)
+    data_sample += data_sample_mean #add DC offset back
+    return data_sample
+
+def amplitude_adjust(data_sample, amplitude):
+    data_sample = data_sample*(random.uniform(1-amplitude, 1+amplitude))
+    return data_sample
 
 def batch_generator(data, data_length, batch_size):
     while True:
